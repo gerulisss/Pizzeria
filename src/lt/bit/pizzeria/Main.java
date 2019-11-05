74a9972e77247b14d12e37700f6e9bec1fd5371d
RokisM <gerulisss@users.noreply.github.com>
2019-11-05 15:58:41 +0200

    basic structure

--- src/lt/bit/pizzeria/Main.java
+++ src/lt/bit/pizzeria/Main.java
@@ -3,6 +3,6 @@ package lt.bit.pizzeria;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
--- src/lt/bit/pizzeria/MenuItem.java
+++ src/lt/bit/pizzeria/MenuItem.java
@@ -1,4 +1,5 @@
 package lt.bit.pizzeria;
 
 public interface MenuItem {
+
 }
